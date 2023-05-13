e8dbd0832b145c54ad32607ffe2a622dea8bc293
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-13 23:10:53 +0500

    Minor Changes

--- stepxx_web3/step06_dapps/step01_connectWallet/src/components/connectButton/index.tsx
+++ stepxx_web3/step06_dapps/step01_connectWallet/src/components/connectButton/index.tsx
@@ -1,72 +1,61 @@
 "use client";
 
-import React, { useMemo, useState } from 'react'
-import { getZeroDevSigner, getSocialWalletOwner, ZeroDevSigner } from '@zerodevapp/sdk'
-import {
-  SocialWallet,
-} from '@zerodevapp/social-wallet';
-
+import React, { useMemo, useState } from 'react';
+import { getZeroDevSigner, getSocialWalletOwner } from '@zerodevapp/sdk';
+import { SocialWallet } from '@zerodevapp/social-wallet';
 
 declare global {
   interface Window {
     ethereum: any;
   }
-}
+};
 
 function ConnectButton() {
 
+  const [address, setAddress] = useState<string>();
 
+  const [loading, setLoading] = useState(false);
 
-  const [address, setAddress] = useState<string>()
-  const [loading, setLoading] = useState(false)
-
-
-  console.log("user ", address)
-
+  console.log("user ", address);
 
   const socialWallet = useMemo(() => {
     return new SocialWallet()
-  }, [])
-
+  }, []);
 
   const createWallet = async () => {
 
     try {
 
-      setLoading(true)
+      setLoading(true);
 
       const signer = await getZeroDevSigner({
         projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6',
         owner: await getSocialWalletOwner('b5486fa4-e3d9-450b-8428-646e757c10f6', socialWallet)
-      })
-
+      });
 
       const userAddress = await signer.getAddress();
-      setAddress(userAddress)
+      setAddress(userAddress);
 
     }
     catch (e) {
-
-      console.log(e)
+      console.log(e);
     }
     finally {
-      setLoading(false)
+      setLoading(false);
     }
-
-  }
-
+    
+  };
 
   const disconnect = async () => {
+
     await socialWallet.disconnect();
-    setAddress(undefined)
 
+    setAddress(undefined);
 
-  }
-
+  };
 
   return (
     <div>
-
       <div>
         {!address && <button className='bg-gray-300 p-3' onClick={createWallet} disabled={loading}>{loading ? 'loading...' : 'Connect Wallet'}</button>}
         {!!address &&
@@ -80,6 +69,6 @@ function ConnectButton() {
       }
     </div>
   )
-}
+};
 
-export default ConnectButton
+export default ConnectButton;
